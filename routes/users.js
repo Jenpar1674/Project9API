61e4fb6037088f93bb4377189644a4d2a723654d
Jennifer Parker <jennifer.parker1@verizon.com>
2019-09-05 16:05:22 -0500

    final of day

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+// const User = require("../models").Users;
 
+function asyncHandler(cb){
+  return async (req,res, next) => {
+      try {
+          await cb(req, res, next);
+      } catch(err) {
+          next(err);
+      }
+  }
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
